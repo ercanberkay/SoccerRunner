13f7c2fc650983db19d744880b650d6e02b133a5
ercanberkay <130152488+ercanberkay@users.noreply.github.com>
2023-07-09 19:01:50 +0300

    Initial commit

---
+++ Assets/Scripts/GamePlay/PlayerControl/PlayerCollideControl.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UIControl;
+using DG.Tweening;
+
+
+namespace CharacterControl 
+{
+    public class PlayerCollideControl : MonoBehaviour
+    {
+        void Start()
+        {
+            RigidbodyActivator(true);
+            ColliderActivator(false);
+        }
+
+        void RigidbodyActivator(bool rbStatus)
+        {
+            Rigidbody[] rb = transform.parent.transform.GetChild(1).GetComponentsInChildren<Rigidbody>();
+            foreach (Rigidbody item in rb)
+            {
+                item.isKinematic = rbStatus;
+            }
+        }
+
+        void ColliderActivator(bool colStatus)
+        {
+            Collider[] cl = transform.parent.transform.GetChild(1).GetComponentsInChildren<Collider>();
+            foreach (Collider item in cl)
+            {
+                item.enabled = colStatus;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Interactable interactable = other.GetComponent<Interactable>();
+            if (interactable!=null)
+            {
+                if (interactable.type == InteractableType.Enemy)
+                {
+                    StartCoroutine(EnemyCollision());
+                }
+                if (interactable.type == InteractableType.EndChunk)
+                {
+                    Vector3 pos = new Vector3(other.transform.position.x-1, other.transform.position.y, other.transform.position.z);
+                    LevelCompleteFunc(pos);
+                }
+                if (interactable.type == InteractableType.Friend)
+                {
+                    PlayerMovement.instance.playerSpeed = 7;
+                }
+            }
+            
+        }
+
+
+        IEnumerator EnemyCollision()
+        {
+            ColliderActivator(true);
+            RigidbodyActivator(false);
+
+            LevelManager.gameState = GameState.Finish;
+            PlayerMovement.instance.isLose = true;
+            PlayerMovement.instance.playerSpeed = 0;
+            PlayerAnimController.instance.animator.enabled = false;
+
+            DuringGame.instance.transform.GetChild(0).gameObject.SetActive(false);
+
+
+            yield return new WaitForSeconds(.5f);
+
+            FailPanel.instance.transform.GetChild(0).gameObject.SetActive(true);
+
+        }
+
+
+        void LevelCompleteFunc(Vector3 center)
+        {
+            LevelManager.gameState = GameState.Finish;
+
+            DuringGame.instance.transform.GetChild(0).gameObject.SetActive(false);
+            CompletePanel.instance.completePanel.SetActive(true);
+
+            CinemachineController.instance.FovAdder(30);
+            PlayerMovement.instance.transform.DOMove(center, .5f);
+            PlayerMovement.instance.playerSpeed = 0;
+
+            string[] winAnims = new string[3];
+            winAnims[0] = "Win1";
+            winAnims[1] = "Win2";
+            winAnims[2] = "Win3";
+            PlayerAnimController.instance.animator.Play(winAnims[Random.Range(0,2)]);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CharacterControl;
+
+public class SpawnManager : MonoBehaviour
+{
+    #region Singleton Pattern
+    public static SpawnManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+
+    [Header ("Prefabs")]
+    public GameObject friendPrefab;
+    public GameObject enemyPrefab;
+    public GameObject chunkPrefab;
+    public GameObject levelEndPrefab;
+
+    [Header("Parameters")]
+    public int chunkCount = 10;
+    public int enemyCount = 15;
+
+    [Header("Vectors")]
+    Vector3 enemySpawnPos;
+
+    void Start()
+    {
+        InstantiateChunks();
+        InstantiateEnemy();
+        InstantiateFriend();
+        enemySpawnPos = new Vector3(-1.5f ,0,0) + transform.GetChild(0).GetChild(chunkCount - 1).position;
+    }
+
+    void InstantiateChunks()
+    {
+        Vector3 pos = new Vector3(0, 0, chunkPrefab.GetComponent<Renderer>().bounds.size.z);
+        for (int i = 0; i <= chunkCount; i++)
+        {
+            if (i == chunkCount)
+            {
+                GameObject myEndChunk = Instantiate(levelEndPrefab, pos * i, Quaternion.identity, transform.GetChild(0));
+            }
+            else
+            {
+                GameObject myChunk = Instantiate(chunkPrefab, pos * i, Quaternion.identity, transform.GetChild(0));
+            }
+        }
+    }
+
+    void InstantiateEnemy()
+    {
+
+        for (int i = 1; i < chunkCount ; i++)
+        {
+            Vector3 pos = new Vector3(0, 0,Random.Range(10,20));
+            GameObject myEnemy = Instantiate(enemyPrefab, new Vector3(-1.5f,0,0) + pos * i,enemyPrefab.transform.localRotation, transform.GetChild(1));
+        }
+    }
+
+    void InstantiateFriend()
+    {
+        Vector3 pos = new Vector3(0, 0, chunkPrefab.GetComponent<Renderer>().bounds.size.z);
+        for (int i = 1; i < chunkCount; i++)
+        {
+            GameObject myFriend = Instantiate(friendPrefab, new Vector3(1.5f, 0, 0) + pos * i, friendPrefab.transform.localRotation ,transform.GetChild(2));
+        }
+    }
+
+}
